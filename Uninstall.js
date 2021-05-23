fa83a46f0e564dd2868776d24754ca4f3b4790e9
BlueEyesDev <84388093+BlueEyesDev@users.noreply.github.com>
2021-05-24 01:33:01 +0200

    Update Uninstall.js

--- Uninstall.js
+++ Uninstall.js
@@ -1,59 +1,46 @@
 const isAdmin = require('is-admin');
-const fs = require('fs');
-const Hosts = [
-    '127.0.0.1	eu1.ethermine.org',
-    '127.0.0.1	asia1.ethermine.org',
-    '127.0.0.1	us1.ethermine.org',
-    '127.0.0.1	us2.ethermine.org',
-    '127.0.0.1	asia1.ethpool.org',
-    '127.0.0.1	eu1.ethpool.org',
-    '127.0.0.1	us1.ethpool.org',
-    '127.0.0.1	us2.ethpool.org',
-    '127.0.0.1	eth-eu1.nanopool.org',
-    '127.0.0.1	eth-eu2.nanopool.org',
-    '127.0.0.1	eth-us-east1.nanopool.org',
-    '127.0.0.1	eth-us-west1.nanopool.org',
-    '127.0.0.1	eth-asia1.nanopool.org',
-    '127.0.0.1	eth-jp1.nanopool.org',
-    '127.0.0.1	eth-au1.nanopool.org',
-    '127.0.0.1	eth.2miners.com',
-    '127.0.0.1	us-eth.2miners.com',
-    '127.0.0.1	asia-eth.2miners.com',
-    '127.0.0.1	cn.sparkpool.com',
-    '127.0.0.1	asia.sparkpool.com',
-    '127.0.0.1	eth-us.sparkpool.com',
-    '127.0.0.1	eth-eu.sparkpool.com'
-];
-if (fs.existsSync('.Installed')){
+const {existsSync, createWriteStream, readFileSync, unlinkSync, writeFileSync} = require('fs');
+const {EOL, platform} = require('os');
+const ReadFilePoolUrls = JSON.parse(readFileSync(`PoolsUrl.json`, 'utf8'));
+const ConsoleLog = (Message, Error) => {
+    if (Error === true){
+        console.error(`\x1b[31m${Message}\x1b[37m`);
+        process.exit();
+    } else
+        console.log(`\x1b[32m${Message}\x1b[37m`);
+}
+const WriteHosts = (Path) => {
+    const data = readFileSync(Path, 'utf8').split(EOL);   
     const HostsFile = [];
-    if (process.platform === "win32"){
+    data.forEach(line => {
+        if (ReadFilePoolUrls.findIndex(Domain => line.includes(Domain)) == -1)
+            HostsFile.push(line);
+    });
+    writeFileSync(Path, HostsFile.join(EOL));
+}
+if (!existsSync('PoolsUrl.json')){
+    const file = createWriteStream("PoolsUrl.json");
+    get("https://raw.githubusercontent.com/BlueEyesDev/PhoenixMiner-NoDevFee/main/PoolsUrl.json", (response) => { response.pipe(file); });
+}
+if (!existsSync('Pools.json')){
+    const file = createWriteStream("Pools.json");
+    get("https://raw.githubusercontent.com/BlueEyesDev/PhoenixMiner-NoDevFee/main/Pools.json", (response) => { response.pipe(file); });
+}
+if (existsSync('.Installed')){
+    if (platform() === "win32"){
         (async () => {
-            if (await isAdmin() === false){ 
-                console.error(`\x1b[31mPlease run the program as Administrator\x1b[37m`);
-                process.exit();
-            } else {
-                const data = fs.readFileSync(`${process.env.DriverData}\\..\\etc\\hosts`, 'utf8').split('\r\n');
-                data.forEach(element => {
-                    if (!Hosts.includes(element))
-                        HostsFile.push(element);
-                });
-                fs.writeFileSync(`${process.env.DriverData}\\..\\etc\\hosts`, HostsFile.join('\r\n'));
-            }
+            if (await isAdmin() === false) 
+                ConsoleLog(`Please run the program as Administrator`, true);
+            else
+                WriteHosts(`${process.env.DriverData}\\..\\etc\\hosts`);
         })();
     } else {
         if (process.env.SUDO_UID == undefined){
-            console.error(`\x1b[31mPlease run the program as Sudo\x1b[37m`);
-            process.exit();
+            ConsoleLog(`Please run the program as Sudo`, true);
         } else {
-            const data = fs.readFileSync(`/etc/hosts`, 'utf8').split('\r\n');
-            data.forEach(element => {
-                if (!Hosts.includes(element))
-                    HostsFile.push(element);
-            });
-            console.log(HostsFile);
-           fs.writeFileSync(`/etc/hosts`, HostsFile.join('\r\n'));
+            WriteHosts(`/etc/hosts`);
         }
     }
-    fs.unlinkSync('.Installed');
+    unlinkSync('.Installed');
 }
-console.log('\x1b[32mUninstall successful\x1b[37m');
+ConsoleLog(`Uninstall successful`, false);
